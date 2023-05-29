48f92890c717eabcefbb63ae84ff28a9f96ec04d
Juanjo1809 <63309174+Juanjo1809@users.noreply.github.com>
2023-05-29 12:01:12 +0200

    footer page

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import './footer.css'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>EGATOR</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experiences">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a href="https://linkedin.com"><BsLinkedin/></a>
+        <a href="https://github.com"><FaGithub/></a>
+      </div>
+      
+      <div className="footer__copyright">
+        <small>&copy; Juan José CERVILLA Website Portfolio. All right reserved.</small>
+      </div>
+
+    </footer>
   )
 }
 
